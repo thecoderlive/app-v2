9b360357784ae203accfff3159ecfaeb30016cdb
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 12:04:03 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "ebdf0660-3496-46fa-834a-245522a206bc",
+    "home_feed": [
+        {
+            "id": "142711eb-b912-4b52-8d9c-d437f92d02b9",
+            "artist_name": "natural",
+            "home_carousel": [
+                {
+                    "id": "0a2cf6ba-bc0c-43e9-9c51-5fbd106641a8",
+                    "art_photo": "https://images.unsplash.com/photo-1611176131649-c2c6499389bf?w=1080"
+                },
+                {
+                    "id": "1bc0dced-a6b0-4413-9f3a-a5e6af3aaef1",
+                    "art_photo": "https://images.unsplash.com/photo-1562070303-d17078411902?w=1080"
+                },
+                {
+                    "id": "130e05dc-a13a-471c-bd77-41ce2f1e364d",
+                    "art_photo": "https://images.unsplash.com/photo-1633119272240-97162812eb63?w=1080"
+                },
+                {
+                    "id": "56488fbd-065e-42d2-b9fc-1b5e4f54a549",
+                    "art_photo": "https://images.unsplash.com/photo-1467318339494-a32c2ada54d4?w=1080"
+                },
+                {
+                    "id": "72597ed7-3e5b-4ba3-ab91-f7219b97bac3",
+                    "art_photo": "https://images.unsplash.com/photo-1612957803626-1bf65224ff68?w=1080"
+                }
+            ],
+            "art_caption": "entire",
+            "art_description": "Carry will sing commercial necessary if consider. Candidate marriage individual let draw experience including listen."
+        },
+        {
+            "id": "774cfdb6-cf91-489f-9dd3-fb1e4ffb1fe4",
+            "artist_name": "crime",
+            "home_carousel": [
+                {
+                    "id": "1f60b24b-1a6a-44a3-a33e-3b109fff56ad",
+                    "art_photo": "https://images.unsplash.com/photo-1552053831-71594a27632d?w=1080"
+                },
+                {
+                    "id": "f10eb773-c774-42be-8afa-c4f2189f6b0a",
+                    "art_photo": "https://images.unsplash.com/photo-1596273501583-5bdb2c7220d9?w=1080"
+                },
+                {
+                    "id": "68707773-7158-4584-8cef-ab8476df6036",
+                    "art_photo": "https://images.unsplash.com/photo-1601640365825-66327247a242?w=1080"
+                },
+                {
+                    "id": "a8ed9a91-5c37-4e1b-8247-b2e4ee1b496f",
+                    "art_photo": "https://images.unsplash.com/photo-1593176848777-70ccd1abb412?w=1080"
+                },
+                {
+                    "id": "38bce4f3-2ac3-4345-a505-0caca4354b1c",
+                    "art_photo": "https://images.unsplash.com/photo-1476129703232-c8806852600f?w=1080"
+                }
+            ],
+            "art_caption": "understand",
+            "art_description": "In purpose responsibility those campaign somebody. Happen rule color into cold. Join say join."
+        },
+        {
+            "id": "3c2296e2-79d9-4308-9960-fc7e1be89464",
+            "artist_name": "around",
+            "home_carousel": [
+                {
+                    "id": "ada6db53-7b8e-4797-8d9f-b635694a1695",
+                    "art_photo": "https://images.unsplash.com/photo-1555922927-32479f120fbf?w=1080"
+                },
+                {
+                    "id": "df991b1a-8db6-4173-b843-68b2db493282",
+                    "art_photo": "https://images.unsplash.com/photo-1528747354767-89476f71d2a3?w=1080"
+                },
+                {
+                    "id": "761a2cb6-49e5-4458-a578-9f629ae0093a",
+                    "art_photo": "https://images.unsplash.com/photo-1454269691289-5a247de3b723?w=1080"
+                },
+                {
+                    "id": "19ad620b-b584-42df-8b70-af581f898535",
+                    "art_photo": "https://images.unsplash.com/photo-1569873985131-0357cc36c027?w=1080"
+                },
+                {
+                    "id": "6df89c3d-bbc8-4b08-9810-43e2bc62294a",
+                    "art_photo": "https://images.unsplash.com/photo-1644227661648-4c1953d77e9d?w=1080"
+                }
+            ],
+            "art_caption": "economy",
+            "art_description": "Without scientist expect know. Middle him meeting seem garden degree event size."
+        },
+        {
+            "id": "799ce205-04ea-4be3-9094-bd6b7a9cb5e6",
+            "artist_name": "drive",
+            "home_carousel": [
+                {
+                    "id": "e45746e6-4343-4877-bce5-bd0992ef72be",
+                    "art_photo": "https://images.unsplash.com/photo-1614844848029-058f34a0508b?w=1080"
+                },
+                {
+                    "id": "a7cc15ab-61cb-401a-8c7b-ea171f8bb3c0",
+                    "art_photo": "https://images.unsplash.com/flagged/photo-1564228539519-4dfe919785a6?w=1080"
+                },
+                {
+                    "id": "49bccd9c-c9c9-4e0f-a587-6641007ad4d1",
+                    "art_photo": "https://images.unsplash.com/photo-1585369111972-771555a164c5?w=1080"
+                },
+                {
+                    "id": "88662a86-4362-43de-afba-6f840fa663a8",
+                    "art_photo": "https://images.unsplash.com/photo-1606133261539-5eb690ae1248?w=1080"
+                },
+                {
+                    "id": "b8d6686f-c946-4595-a829-fe4a03755f19",
+                    "art_photo": "https://images.unsplash.com/photo-1510566255260-5fff11a0c999?w=1080"
+                }
+            ],
+            "art_caption": "walk",
+            "art_description": "Stage street those country behind forget similar. Charge city perhaps build. Natural since truth example range cup black."
+        },
+        {
+            "id": "55684ebb-5d3e-4f41-bd1a-d2ff4e12f1e5",
+            "artist_name": "when",
+            "home_carousel": [
+                {
+                    "id": "29634bd4-1be1-442b-bf97-3338a0d51827",
+                    "art_photo": "https://images.unsplash.com/photo-1635354701147-3b8e8210c438?w=1080"
+                },
+                {
+                    "id": "d709752a-50c0-4856-8489-28be03b6b341",
+                    "art_photo": "https://images.unsplash.com/photo-1506790144-fe3c68e4247d?w=1080"
+                },
+                {
+                    "id": "c68ac68e-29bb-4bc2-a26b-848780c9d97d",
+                    "art_photo": "https://images.unsplash.com/photo-1523048970790-3db7ed9e6b6f?w=1080"
+                },
+                {
+                    "id": "077a0bee-9a59-4cea-af01-756e9f308a6d",
+                    "art_photo": "https://images.unsplash.com/photo-1638987158215-dbaa874ed068?w=1080"
+                },
+                {
+                    "id": "3600719f-3ffc-476f-8724-83378b9baef8",
+                    "art_photo": "https://images.unsplash.com/photo-1593319627599-10ce6d525183?w=1080"
+                }
+            ],
+            "art_caption": "support",
+            "art_description": "Family laugh music."
+        }
+    ]
+}
